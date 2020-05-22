cfd0788031a93ed41373c7edd29f0bcb4120f9f9
PedroA <52288792+Pedr0735@users.noreply.github.com>
2020-05-22 14:23:19 -0300

    Backend da aplição

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 // Conecta no MongoDB
   mongoose.connect('mongodb+srv://PedroA:p5kseasus@cluster0-08d87.mongodb.net/test?retryWrites=true&w=majority', {autoIndex: false})
 
 // Carrega o model de Usuário
 require("./models/user");
+
+app.use(cors())
 
 app.use(bodyParser.json());
 
